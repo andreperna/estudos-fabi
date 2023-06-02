f96aece463920dc29500130a6bb445c6bc972b18
andreperna <andreperna@gmail.com>
2023-06-01 23:05:48 -0300

    update .DS_Store, index.html, script.js and style.css

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ function toggleMode() {
   // referencia img
   const img = document.querySelector("#profile img")
 
-  if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar-light.png")
-  } else {
-    img.setAttribute("src", "./assets/avatar-dark.png")
-  }
+  // if (html.classList.contains("light")) {
+  //   img.setAttribute("src", "./assets/fabi.png")
+  // } else {
+  //   img.setAttribute("src", "./assets/fabi.png")
+  // }
 }
